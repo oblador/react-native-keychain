e507c99b9914a97332f98284ac0c153a07450de1
Joel Arvidsson <joel@oblador.se>
2016-11-02 21:42:55 +0100

    Update example to RN 0.36 & ES 2016

---
+++ KeychainExample/app.js
@@ -0,0 +1,166 @@
+import React, { Component } from 'react';
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+
+import Keychain from 'react-native-keychain';
+
+export default class KeychainExample extends Component {
+  state = {
+    username: '',
+    password: '',
+    status: '',
+  };
+
+  save() {
+    Keychain
+      .setGenericPassword(this.state.username, this.state.password)
+      .then(() => {
+        this.setState({status: 'Credentials saved!'});
+      })
+      .catch((err) => {
+        this.setState({status: 'Could not save credentials, ' + err});
+      });
+  }
+
+  load() {
+    Keychain
+      .getGenericPassword()
+      .then((credentials) => {
+        this.setState(credentials);
+        this.setState({status: 'Credentials loaded!'});
+      })
+      .catch((err) => {
+        this.setState({status: 'Could not load credentials. ' + err});
+      });
+  }
+
+  reset() {
+    Keychain
+      .resetGenericPassword()
+      .then(() => {
+        this.setState({status: 'Credentials Reset!', username: '', password: '' });
+      })
+      .catch((err) => {
+        this.setState({status: 'Could not reset credentials, ' + err});
+      });
+  }
+
+  render() {
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            Keychain Example
+          </Text>
+          <View style={styles.field}>
+            <Text style={styles.label}>Username</Text>
+            <TextInput
+              style={styles.input}
+              autoFocus={true}
+              autoCapitalize="none"
+              value={this.state.username}
+              onChange={(event) => this.setState({ username: event.nativeEvent.text })}
+             />
+          </View>
+          <View style={styles.field}>
+            <Text style={styles.label}>Password</Text>
+            <TextInput
+              style={styles.input}
+              password={true}
+              autoCapitalize="none"
+              value={this.state.password}
+              onChange={(event) => this.setState({ password: event.nativeEvent.text })}
+             />
+          </View>
+          {!!this.state.status && (<Text style={styles.status}>{this.state.status}</Text>)}
+          <View style={styles.buttons}>
+            <TouchableHighlight onPress={() => this.save()} style={styles.button}>
+              <View style={styles.save}>
+                <Text style={styles.buttonText}>Save</Text>
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={() => this.load()} style={styles.button}>
+              <View style={styles.load}>
+                <Text style={styles.buttonText}>Load</Text>
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={() => this.reset()} style={styles.button}>
+              <View style={styles.reset}>
+                <Text style={styles.buttonText}>Reset</Text>
+              </View>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  content: {
+    width: 250,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '200',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  field: {
+    marginVertical: 5,
+  },
+  label: {
+    fontWeight: '500',
+    fontSize: 15,
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ccc',
+    backgroundColor: 'white',
+    height: 32,
+    fontSize: 14,
+    padding: 8,
+  },
+  status: {
+    color: '#333',
+    fontSize: 12,
+    marginTop: 15,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  button: {
+    borderRadius: 3,
+    overflow: 'hidden',
+  },
+  save: {
+    backgroundColor: '#0c0',
+  },
+  load: {
+    backgroundColor: '#333',
+  },
+  reset: {
+    backgroundColor: '#c00',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 14,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  }
+});
