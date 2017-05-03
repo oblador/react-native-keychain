c394ddbfabf1d69185753bd033fa19b47f9be205
Vojtech Novak <vonovak@gmail.com>
2017-05-03 17:36:47 +0200

    [android] upgrade conceal, minor refactoring

--- android/src/main/java/com/oblador/keychain/KeychainModule.java
+++ android/src/main/java/com/oblador/keychain/KeychainModule.java
@@ -5,11 +5,14 @@ import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.util.Base64;
 import android.util.Log;
+
 import com.facebook.android.crypto.keychain.AndroidConceal;
 import com.facebook.android.crypto.keychain.SharedPrefsBackedKeyChain;
 import com.facebook.crypto.Crypto;
 import com.facebook.crypto.CryptoConfig;
 import com.facebook.crypto.Entity;
+import com.facebook.crypto.exception.CryptoInitializationException;
+import com.facebook.crypto.exception.KeyChainException;
 import com.facebook.crypto.keychain.KeyChain;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
@@ -19,6 +22,7 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 
 public class KeychainModule extends ReactContextBaseJavaModule {
@@ -47,41 +51,37 @@ public class KeychainModule extends ReactContextBaseJavaModule {
     public void setGenericPasswordForOptions(String service, String username, String password, Promise promise) {
         if (!crypto.isAvailable()) {
             Log.e(KEYCHAIN_MODULE, "Crypto is missing");
-            promise.reject("KeychainModule: crypto is missing");
+            promise.reject(KEYCHAIN_MODULE, "crypto is missing");
             return;
         }
         if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
             Log.e(KEYCHAIN_MODULE, "you passed empty or null username/password");
-            promise.reject("KeychainModule: you passed empty or null username/password");
+            promise.reject(KEYCHAIN_MODULE, "you passed empty or null username/password");
             return;
         }
         service = service == null ? EMPTY_STRING : service;
-        //Log.d("Crypto", service + username + password);
 
-        Entity userentity = Entity.create(KEYCHAIN_DATA + ":" + service + "user");
-        Entity pwentity = Entity.create(KEYCHAIN_DATA + ":" + service + "pass");
+        Entity userEntity = Entity.create(KEYCHAIN_DATA + ":" + service + "user");
+        Entity passwordEntity = Entity.create(KEYCHAIN_DATA + ":" + service + "pass");
 
+        try {
+            String encryptedUsername = encryptWithEntity(username, userEntity);
+            String encryptedPassword = encryptWithEntity(password, passwordEntity);
 
-        String encryptedUsername = encryptWithEntity(username, userentity, promise);
-        String encryptedPassword = encryptWithEntity(password, pwentity, promise);
-
-        SharedPreferences.Editor prefsEditor = prefs.edit();
-        prefsEditor.putString(service + ":u", encryptedUsername);
-        prefsEditor.putString(service + ":p", encryptedPassword);
-        prefsEditor.apply();
-        Log.d(KEYCHAIN_MODULE, "saved the data");
-        promise.resolve("KeychainModule saved the data");
+            SharedPreferences.Editor prefsEditor = prefs.edit();
+            prefsEditor.putString(service + ":u", encryptedUsername);
+            prefsEditor.putString(service + ":p", encryptedPassword);
+            prefsEditor.apply();
+            promise.resolve("KeychainModule saved the data");
+        } catch (KeyChainException | IOException | CryptoInitializationException e) {
+            Log.e(KEYCHAIN_MODULE, e.getLocalizedMessage());
+            promise.reject(KEYCHAIN_MODULE, e);
+        }
     }
 
-    private String encryptWithEntity(String toEncypt, Entity entity, Promise promise) {
-        try {
-            byte[] encryptedBytes = crypto.encrypt(toEncypt.getBytes(Charset.forName("UTF-8")), entity);
-            return Base64.encodeToString(encryptedBytes, Base64.DEFAULT);
-        } catch (Exception e) {
-            Log.e(KEYCHAIN_MODULE, e.getLocalizedMessage());
-            promise.reject(e.getLocalizedMessage(), e);
-            return null;
-        }
+    private String encryptWithEntity(String toEncypt, Entity entity) throws KeyChainException, CryptoInitializationException, IOException {
+        byte[] encryptedBytes = crypto.encrypt(toEncypt.getBytes(Charset.forName("UTF-8")), entity);
+        return Base64.encodeToString(encryptedBytes, Base64.DEFAULT);
     }
 
     @ReactMethod
@@ -96,15 +96,15 @@ public class KeychainModule extends ReactContextBaseJavaModule {
             return;
         }
 
-        byte[] recuser = Base64.decode(username, Base64.DEFAULT);
-        byte[] recpass = Base64.decode(password, Base64.DEFAULT);
+        Entity userEntity = Entity.create(KEYCHAIN_DATA + ":" + service + "user");
+        Entity passwordEntity = Entity.create(KEYCHAIN_DATA + ":" + service + "pass");
 
-        Entity userentity = Entity.create(KEYCHAIN_DATA + ":" + service + "user");
-        Entity pwentity = Entity.create(KEYCHAIN_DATA + ":" + service + "pass");
+        byte[] receivedUser = Base64.decode(username, Base64.DEFAULT);
+        byte[] receivedPass = Base64.decode(password, Base64.DEFAULT);
 
         try {
-            byte[] decryptedUsername = crypto.decrypt(recuser, userentity);
-            byte[] decryptedPass = crypto.decrypt(recpass, pwentity);
+            byte[] decryptedUsername = crypto.decrypt(receivedUser, userEntity);
+            byte[] decryptedPass = crypto.decrypt(receivedPass, passwordEntity);
 
             WritableMap credentials = Arguments.createMap();
 
@@ -113,9 +113,9 @@ public class KeychainModule extends ReactContextBaseJavaModule {
             credentials.putString("password", new String(decryptedPass, Charset.forName("UTF-8")));
 
             promise.resolve(credentials);
-        } catch (Exception e) {
+        } catch (KeyChainException | IOException | CryptoInitializationException e) {
             Log.e(KEYCHAIN_MODULE, e.getLocalizedMessage());
-            promise.reject(e.getLocalizedMessage(), e);
+            promise.reject(KEYCHAIN_MODULE, e);
         }
     }
 
@@ -146,6 +146,5 @@ public class KeychainModule extends ReactContextBaseJavaModule {
     public void resetInternetCredentialsForServer(@NonNull String server, ReadableMap unusedOptions, Promise promise) {
         resetGenericPasswordForOptions(server, promise);
     }
-
 
 }
