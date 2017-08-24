712679ff892a386f1ad678ed519bb390539304a7
Julien K <JulienKode@users.noreply.github.com>
2017-08-24 23:21:36 +0200

    Add react-native-keychain on tvOS  (#78)
    * [tvOS] Update podspec
    
    * [tvOS] Add target
    
    * [SecRequestSharedWebCredential] is only available on iOS and not tvOS
    
    * [Security] Move into the same folder Framework
    
    * [RNKeychainManager.m] Change indentation

--- RNKeychainManager/RNKeychainManager.m
+++ RNKeychainManager/RNKeychainManager.m
@@ -285,6 +285,7 @@ RCT_EXPORT_METHOD(resetInternetCredentialsForServer:(NSString *)server withOptio
 
 }
 
+#if TARGET_OS_IOS
 RCT_EXPORT_METHOD(requestSharedWebCredentials:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
   SecRequestSharedWebCredential(NULL, NULL, ^(CFArrayRef credentials, CFErrorRef error) {
@@ -328,5 +329,6 @@ RCT_EXPORT_METHOD(setSharedWebCredentialsForServer:(NSString *)server withUserna
     resolve(@(YES));
   });
 }
+#endif
 
 @end
