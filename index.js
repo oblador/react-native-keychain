d6a00596c48d333e30552d2f192a1c6c62838770
Joel Arvidsson <joel@oblador.se>
2018-02-25 17:29:39 +0100

    Export enum options

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 import { NativeModules, Platform } from 'react-native';
 const { RNKeychainManager } = NativeModules;
 
+export const ACCESSIBLE = {
+  WHEN_UNLOCKED: 'AccessibleWhenUnlocked',
+  AFTER_FIRST_UNLOCK: 'AccessibleAfterFirstUnlock',
+  ALWAYS: 'AccessibleAlways',
+  WHEN_PASSCODE_SET_THIS_DEVICE_ONLY: 'AccessibleWhenPasscodeSetThisDeviceOnly',
+  WHEN_UNLOCKED_THIS_DEVICE_ONLY: 'AccessibleWhenUnlockedThisDeviceOnly',
+  AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY:
+    'AccessibleAfterFirstUnlockThisDeviceOnly',
+  ALWAYS_THIS_DEVICE_ONLY: 'AccessibleAlwaysThisDeviceOnly',
+};
+
+export const ACCESS_CONTROL = {
+  USER_PRESENCE: 'UserPresence',
+  TOUCH_ID_ANY: 'TouchIDAny',
+  TOUCH_ID_CURRENT_SET: 'TouchIDCurrentSet',
+  DEVICE_PASSCODE: 'DevicePasscode',
+  TOUCH_ID_ANY_OR_DEVICE_PASSCODE: 'TouchIDAnyOrDevicePasscode',
+  TOUCH_ID_CURRENT_SET_OR_DEVICE_PASSCODE: 'TouchIDCurrentSetOrDevicePasscode',
+};
+
+export const AUTHENTICATION_TYPE = {
+  DEVICE_PASSCODE_OR_BIOMETRICS: 'AuthenticationWithBiometricsDevicePasscode',
+  BIOMETRICS: 'AuthenticationWithBiometrics',
+};
+
 export const BIOMETRY_TYPE = {
   TOUCH_ID: 'TouchID',
   FACE_ID: 'FaceID',
