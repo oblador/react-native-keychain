b4106fc10b7686c86a6af2965ba62edc3d44c905
David Narbutovich(g) <vamnemne@gmail.com>
2018-12-19 20:48:03 +0300

    Add flow anotation (#163)
    * Add flow anotation
    
    * Update typed and add flow.js test
    
    * Auto calculation of  Emun values for flow types

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// @flow
 import { NativeModules, Platform } from 'react-native';
 const { RNKeychainManager } = NativeModules;
 
-export const ACCESSIBLE = {
+export const ACCESSIBLE = Object.freeze({
   WHEN_UNLOCKED: 'AccessibleWhenUnlocked',
   AFTER_FIRST_UNLOCK: 'AccessibleAfterFirstUnlock',
   ALWAYS: 'AccessibleAlways',
@@ -10,9 +11,9 @@ export const ACCESSIBLE = {
   AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY:
     'AccessibleAfterFirstUnlockThisDeviceOnly',
   ALWAYS_THIS_DEVICE_ONLY: 'AccessibleAlwaysThisDeviceOnly',
-};
+});
 
-export const ACCESS_CONTROL = {
+export const ACCESS_CONTROL = Object.freeze({
   USER_PRESENCE: 'UserPresence',
   BIOMETRY_ANY: 'BiometryAny',
   BIOMETRY_CURRENT_SET: 'BiometryCurrentSet',
@@ -20,40 +21,26 @@ export const ACCESS_CONTROL = {
   APPLICATION_PASSWORD: 'ApplicationPassword',
   BIOMETRY_ANY_OR_DEVICE_PASSCODE: 'BiometryAnyOrDevicePasscode',
   BIOMETRY_CURRENT_SET_OR_DEVICE_PASSCODE: 'BiometryCurrentSetOrDevicePasscode',
-};
+});
 
-export const AUTHENTICATION_TYPE = {
+export const AUTHENTICATION_TYPE = Object.freeze({
   DEVICE_PASSCODE_OR_BIOMETRICS: 'AuthenticationWithBiometricsDevicePasscode',
   BIOMETRICS: 'AuthenticationWithBiometrics',
-};
+});
 
-export const BIOMETRY_TYPE = {
+export const BIOMETRY_TYPE = Object.freeze({
   TOUCH_ID: 'TouchID',
   FACE_ID: 'FaceID',
   FINGERPRINT: 'Fingerprint',
-};
+});
 
-type SecAccessible =
-  | 'AccessibleWhenUnlocked'
-  | 'AccessibleAfterFirstUnlock'
-  | 'AccessibleAlways'
-  | 'AccessibleWhenPasscodeSetThisDeviceOnly'
-  | 'AccessibleWhenUnlockedThisDeviceOnly'
-  | 'AccessibleAfterFirstUnlockThisDeviceOnly'
-  | 'AccessibleAlwaysThisDeviceOnly';
+export type SecAccessible = $Values<typeof ACCESSIBLE>;
 
-type SecAccessControl =
-  | 'UserPresence'
-  | 'BiometryAny'
-  | 'BiometryCurrentSet'
-  | 'DevicePasscode'
-  | 'ApplicationPassword'
-  | 'BiometryAnyOrDevicePasscode'
-  | 'BiometryCurrentSetOrDevicePasscode';
+export type SecAccessControl = $Values<typeof ACCESS_CONTROL>;
 
-type LAPolicy = 'Authentication' | 'AuthenticationWithBiometrics';
+export type LAPolicy = $Values<typeof AUTHENTICATION_TYPE>;
 
-type Options = {
+export type Options = {
   accessControl?: SecAccessControl,
   accessGroup?: string,
   accessible?: SecAccessible,
@@ -68,7 +55,7 @@ type Options = {
  * @param {object} options LAPolicy option, iOS only
  * @return {Promise} Resolves to `true` when supported, otherwise `false`
  */
-export function canImplyAuthentication(options?: Options): Promise {
+export function canImplyAuthentication(options?: Options): Promise<boolean> {
   if (!RNKeychainManager.canCheckAuthentication) {
     return Promise.resolve(false);
   }
@@ -79,7 +66,7 @@ export function canImplyAuthentication(options?: Options): Promise {
  * Get what type of hardware biometry support the device has.
  * @return {Promise} Resolves to a `BIOMETRY_TYPE` when supported, otherwise `null`
  */
-export function getSupportedBiometryType(): Promise {
+export function getSupportedBiometryType(): Promise<?($Values<typeof BIOMETRY_TYPE>)> {
   if (!RNKeychainManager.getSupportedBiometryType) {
     return Promise.resolve(null);
   }
@@ -99,7 +86,7 @@ export function setInternetCredentials(
   username: string,
   password: string,
   options?: Options
-): Promise {
+): Promise<void> {
   return RNKeychainManager.setInternetCredentialsForServer(
     server,
     username,
@@ -113,11 +100,14 @@ export function setInternetCredentials(
  * @param {string} server URL to server.
  * @return {Promise} Resolves to `true` when successful
  */
-export function hasInternetCredentials(
-  server: string,
-): Promise {
+export function hasInternetCredentials(server: string): Promise<boolean> {
   return RNKeychainManager.hasInternetCredentialsForServer(server);
 }
+
+export type UserCredentials = {|
+  +username: string,
+  +password: string,
+|};
 
 /**
  * Fetches login combination for `server`.
@@ -128,7 +118,7 @@ export function hasInternetCredentials(
 export function getInternetCredentials(
   server: string,
   options?: Options
-): Promise {
+): Promise<UserCredentials> {
   return RNKeychainManager.getInternetCredentialsForServer(server, options);
 }
 
@@ -141,7 +131,7 @@ export function getInternetCredentials(
 export function resetInternetCredentials(
   server: string,
   options?: Options
-): Promise {
+): Promise<void> {
   return RNKeychainManager.resetInternetCredentialsForServer(server, options);
 }
 
@@ -167,13 +157,19 @@ export function setGenericPassword(
   username: string,
   password: string,
   serviceOrOptions?: string | Options
-): Promise {
+): Promise<boolean> {
   return RNKeychainManager.setGenericPasswordForOptions(
     getOptionsArgument(serviceOrOptions),
     username,
     password
   );
 }
+
+export type SharedWebCredentials = {|
+  +server: string,
+  +username: string,
+  +password: string,
+|};
 
 /**
  * Fetches login combination for `service`.
@@ -182,7 +178,7 @@ export function setGenericPassword(
  */
 export function getGenericPassword(
   serviceOrOptions?: string | Options
-): Promise {
+): Promise<boolean | SharedWebCredentials> {
   return RNKeychainManager.getGenericPasswordForOptions(
     getOptionsArgument(serviceOrOptions)
   );
@@ -195,7 +191,7 @@ export function getGenericPassword(
  */
 export function resetGenericPassword(
   serviceOrOptions?: string | Options
-): Promise {
+): Promise<boolean> {
   return RNKeychainManager.resetGenericPasswordForOptions(
     getOptionsArgument(serviceOrOptions)
   );
@@ -206,7 +202,7 @@ export function resetGenericPassword(
  * @return {Promise} Resolves to `{ server, username, password }` if approved and
  * `false` if denied and throws an error if not supported on platform or there's no shared credentials
  */
-export function requestSharedWebCredentials(): Promise {
+export function requestSharedWebCredentials(): Promise<SharedWebCredentials> {
   if (Platform.OS !== 'ios') {
     return Promise.reject(
       new Error(
@@ -228,7 +224,7 @@ export function setSharedWebCredentials(
   server: string,
   username: string,
   password: string
-): Promise {
+): Promise<void> {
   if (Platform.OS !== 'ios') {
     return Promise.reject(
       new Error(
