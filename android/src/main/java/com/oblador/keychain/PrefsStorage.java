653140d27fadf8d9a9c397e44a807f4ccb852003
Eric Forkosh <eric@narobo.com>
2019-09-16 04:15:37 -0400

    replace android.support. with androidx.  for AndroidX (#226)

--- android/src/main/java/com/oblador/keychain/PrefsStorage.java
+++ android/src/main/java/com/oblador/keychain/PrefsStorage.java
@@ -2,7 +2,7 @@ package com.oblador.keychain;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Base64;
 
 import com.facebook.react.bridge.ReactApplicationContext;
