bdc94fb9408a9834736294c7a5efe642cedf6c65
Oleksandr Kucherenko <oleksandr.kucherenko@klarna.com>
2019-11-06 10:23:08 +0100

    updates

--- KeychainExample/postinstall_cleanup.sh
+++ KeychainExample/postinstall_cleanup.sh
@@ -7,7 +7,7 @@ rm -rf node_modules/react-native-keychain/gradle
 rm -rf node_modules/react-native-keychain/.idea
 rm -rf node_modules/react-native-keychain/.git
 rm -rf node_modules/react-native-keychain/KeychainExample
-rm -rf node_modules/react-native-keychain/KeychainExampleRN59
+rm -rf node_modules/react-native-keychain/node_modules
 rm -rf node_modules/react-native-keychain/android/build
 find . -type f -name *.iml -delete
 
