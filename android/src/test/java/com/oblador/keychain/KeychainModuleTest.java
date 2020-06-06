4cd218d413beac5e1cc0a9652b2b80978a9aae6a
john.y.pazekha <yevgeniy.pazekha@klarna.com>
2020-06-06 13:46:05 +0200

    Renamed `KeychainModuleTest` class so it can be found by CI build tools.

--- android/src/test/java/com/oblador/keychain/KeychainModuleTest.java
+++ android/src/test/java/com/oblador/keychain/KeychainModuleTest.java
@@ -59,7 +59,7 @@ import static org.mockito.Mockito.when;
 import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
-public class KeychainModuleTests {
+public class KeychainModuleTest {
   public static final byte[] BYTES_USERNAME = "username".getBytes();
   public static final byte[] BYTES_PASSWORD = "password".getBytes();
   /**
