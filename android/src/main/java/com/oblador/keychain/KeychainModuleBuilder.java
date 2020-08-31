c7464cf4603d55a8123792cb757359fa3b2c97be
John Y. Pazekha <yevgeniy.pazekha@klarna.com>
2020-08-31 18:39:25 +0200

    Added to `KeychainModuleBuilder` an option to use or skip the warm up.

--- android/src/main/java/com/oblador/keychain/KeychainModuleBuilder.java
+++ android/src/main/java/com/oblador/keychain/KeychainModuleBuilder.java
@@ -3,16 +3,33 @@ package com.oblador.keychain;
 import com.facebook.react.bridge.ReactApplicationContext;
 
 public class KeychainModuleBuilder {
+  public static final boolean DEFAULT_USE_WARM_UP = true;
+
   private ReactApplicationContext reactContext;
+  private boolean useWarmUp = DEFAULT_USE_WARM_UP;
 
   public KeychainModuleBuilder withReactContext(ReactApplicationContext reactContext) {
     this.reactContext = reactContext;
     return this;
   }
 
+  public KeychainModuleBuilder usingWarmUp() {
+    useWarmUp = true;
+    return this;
+  }
+
+  public KeychainModuleBuilder withoutWarmUp() {
+    useWarmUp = false;
+    return this;
+  }
+
   public KeychainModule build() {
     validate();
-    return KeychainModule.withWarming(reactContext);
+    if (useWarmUp) {
+      return KeychainModule.withWarming(reactContext);
+    } else {
+      return new KeychainModule(reactContext);
+    }
   }
 
   private void validate() {
