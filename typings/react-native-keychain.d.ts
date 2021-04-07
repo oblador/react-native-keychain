13a5270ce35472c183ab162f2ddd2e6e0f861c23
Minwoo <naminwoo8@gmail.com>
2021-04-07 15:01:57 +1200

    add synchronized option in Options

--- typings/react-native-keychain.d.ts
+++ typings/react-native-keychain.d.ts
@@ -1,98 +1,87 @@
 declare module 'react-native-keychain' {
+  export interface UserCredentials {
+    username: string;
+    password: string;
+  }
 
-    export interface UserCredentials {
-        username: string;
-        password: string;
-    }
+  export interface SharedWebCredentials {
+    server: string;
+    username: string;
+    password: string;
+  }
 
-    export interface SharedWebCredentials {
-        server: string;
-        username: string;
-        password: string;
-    }
+  export enum SecAccessible {
+    WHEN_UNLOCKED = 'AccessibleWhenUnlocked',
+    AFTER_FIRST_UNLOCK = 'AccessibleAfterFirstUnlock',
+    ALWAYS = 'AccessibleAlways',
+    WHEN_PASSCODE_SET_THIS_DEVICE_ONLY = 'AccessibleWhenPasscodeSetThisDeviceOnly',
+    WHEN_UNLOCKED_THIS_DEVICE_ONLY = 'AccessibleWhenUnlockedThisDeviceOnly',
+    AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY = 'AccessibleAfterFirstUnlockThisDeviceOnly',
+    ALWAYS_THIS_DEVICE_ONLY = 'AccessibleAlwaysThisDeviceOnly',
+  }
 
-    export enum SecAccessible {
-        WHEN_UNLOCKED = "AccessibleWhenUnlocked",
-        AFTER_FIRST_UNLOCK = "AccessibleAfterFirstUnlock",
-        ALWAYS = "AccessibleAlways",
-        WHEN_PASSCODE_SET_THIS_DEVICE_ONLY = "AccessibleWhenPasscodeSetThisDeviceOnly",
-        WHEN_UNLOCKED_THIS_DEVICE_ONLY = "AccessibleWhenUnlockedThisDeviceOnly",
-        AFTER_FIRST_UNLOCK_THIS_DEVICE_ONLY = "AccessibleAfterFirstUnlockThisDeviceOnly",
-        ALWAYS_THIS_DEVICE_ONLY = "AccessibleAlwaysThisDeviceOnly"
-    }
+  export enum SecAccessControl {
+    USER_PRESENCE = 'UserPresence',
+    BIOMETRY_ANY = 'BiometryAny',
+    BIOMETRY_CURRENT_SET = 'BiometryCurrentSet',
+    DEVICE_PASSCODE = 'DevicePasscode',
+    APPLICATION_PASSWORD = 'ApplicationPassword',
+    BIOMETRY_ANY_OR_DEVICE_PASSCODE = 'BiometryAnyOrDevicePasscode',
+    BIOMETRY_CURRENT_SET_OR_DEVICE_PASSCODE = 'BiometryCurrentSetOrDevicePasscode',
+  }
 
-    export enum SecAccessControl {
-        USER_PRESENCE = "UserPresence",
-        BIOMETRY_ANY = "BiometryAny",
-        BIOMETRY_CURRENT_SET = "BiometryCurrentSet",
-        DEVICE_PASSCODE = "DevicePasscode",
-        APPLICATION_PASSWORD = "ApplicationPassword",
-        BIOMETRY_ANY_OR_DEVICE_PASSCODE = "BiometryAnyOrDevicePasscode",
-        BIOMETRY_CURRENT_SET_OR_DEVICE_PASSCODE = "BiometryCurrentSetOrDevicePasscode"
-    }
+  export enum LAPolicy {
+    DEVICE_PASSCODE_OR_BIOMETRICS = 'AuthenticationWithBiometricsDevicePasscode',
+    BIOMETRICS = 'AuthenticationWithBiometrics',
+  }
 
-    export enum LAPolicy {
-        DEVICE_PASSCODE_OR_BIOMETRICS = "AuthenticationWithBiometricsDevicePasscode",
-        BIOMETRICS = "AuthenticationWithBiometrics"
-    }
+  export interface Options {
+    accessControl?: SecAccessControl;
+    accessGroup?: string;
+    accessible?: SecAccessible;
+    authenticationPrompt?: string;
+    authenticationType?: LAPolicy;
+    service?: string;
+    synchronized?: boolean;
+  }
 
-    export interface Options {
-        accessControl?: SecAccessControl;
-        accessGroup?: string;
-        accessible?: SecAccessible;
-        authenticationPrompt?: string;
-        authenticationType?: LAPolicy;
-        service?: string;
-    }
+  function canImplyAuthentication(options?: Options): Promise<boolean>;
 
-    function canImplyAuthentication(
-        options?: Options
-    ): Promise<boolean>;
+  function getSupportedBiometryType(): Promise<string>;
 
-    function getSupportedBiometryType(
-    ): Promise<string>;
+  function setInternetCredentials(
+    server: string,
+    username: string,
+    password: string,
+    options?: Options
+  ): Promise<void>;
 
-    function setInternetCredentials(
-        server: string,
-        username: string,
-        password: string,
-        options?: Options
-    ): Promise<void>;
+  function getInternetCredentials(
+    server: string,
+    options?: Options
+  ): Promise<UserCredentials>;
 
-    function getInternetCredentials(
-        server: string,
-        options?: Options
-    ): Promise<UserCredentials>;
+  function hasInternetCredentials(server: string): Promise<boolean>;
 
-    function hasInternetCredentials(
-        server: string
-    ): Promise<boolean>;
+  function resetInternetCredentials(server: string): Promise<void>;
 
-    function resetInternetCredentials(
-        server: string
-    ): Promise<void>;
+  function setGenericPassword(
+    username: string,
+    password: string,
+    options?: Options
+  ): Promise<boolean>;
 
-    function setGenericPassword(
-        username: string,
-        password: string,
-        options?: Options
-    ): Promise<boolean>;
+  function getGenericPassword(
+    options?: Options
+  ): Promise<boolean | { service: string; username: string; password: string }>;
 
-    function getGenericPassword(
-        options?: Options
-    ): Promise<boolean | {service: string, username: string, password: string}>;
+  function resetGenericPassword(options?: Options): Promise<boolean>;
 
-    function resetGenericPassword(
-        options?: Options
-    ): Promise<boolean>
+  function requestSharedWebCredentials(): Promise<SharedWebCredentials>;
 
-    function requestSharedWebCredentials (
-    ): Promise<SharedWebCredentials>;
-
-    function setSharedWebCredentials(
-        server: string,
-        username: string,
-        password: string
-    ): Promise<void>;
-
+  function setSharedWebCredentials(
+    server: string,
+    username: string,
+    password: string
+  ): Promise<void>;
 }
