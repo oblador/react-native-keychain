ecdc7bd213597ed6e18c0d30173860b5d47fc49a
Bo Wang <bobowinca@gmail.com>
2021-07-22 11:15:20 -0400

    Remove more reference to Drbg parameters

--- android/src/main/java/com/oblador/keychain/cipherStorage/CipherStorageBase.java
+++ android/src/main/java/com/oblador/keychain/cipherStorage/CipherStorageBase.java
@@ -534,12 +534,12 @@ abstract public class CipherStorageBase implements CipherStorage {
     public static final SecureRandom drbgSecureRandom() {
       SecureRandom drbgSecureRandom = null;
       try {
-          drbgSecureRandom = SecureRandom.getInstance("DRBG" , // Uses default configured DRBG mechanism which is usually SHA-256 Hash. Good idea to check in your java.security file.
+          drbgSecureRandom = SecureRandom.getInstance("DRBG" /*, // Uses default configured DRBG mechanism which is usually SHA-256 Hash. Good idea to check in your java.security file.
                   DrbgParameters.instantiation(256, // Security strength, default is 128 as configured in java.security
                                                   PR_AND_RESEED, // prediction resistance, and re-seeding support.
                                                               // Prediction Resistance ==  compromise of the DRBG internal state has no effect on the security of future DRBG outputs.
                                                               // Reseeding == Periodic reseeding, to avoid too many output from a seed
-                                                  "any_hardcoded_string".getBytes()));
+                                                  "any_hardcoded_string".getBytes())*/);
       } catch (NoSuchAlgorithmException e) { System.out.println("DRBG algorithm for generating CSPRNG is not supported"); }
 
       return drbgSecureRandom;
