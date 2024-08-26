bb0caec2f9e4f998892741bb0b4cb0f9285d10e5
MazurDorian <mazur.dorian15@gmail.com>
2024-08-26 13:52:15 +0300

    fix: enrollFingerprint script for android

--- KeychainExample/e2e/utils/enrollFingerprintAndroid.sh
+++ KeychainExample/e2e/utils/enrollFingerprintAndroid.sh
@@ -1,32 +1,22 @@
 #!/usr/bin/env bash
 
-DELAY=10
+DELAY=3
 adb shell locksettings set-pin 1111
 adb shell am start -a android.settings.SECURITY_SETTINGS
 sleep $DELAY
-adb shell input tap 274 2300
+adb shell input tap 274 1150
 sleep $DELAY
 adb shell input text 1111
 adb shell input keyevent 66
 sleep $DELAY
-adb shell input tap 1100 2700
+adb shell input tap 900 2200
 sleep $DELAY
-adb shell input tap 1100 2700
-sleep $DELAY
-adb -e emu finger touch 1
+adb shell input tap 900 2200
 sleep $DELAY
 adb -e emu finger touch 1
 sleep $DELAY
 adb -e emu finger touch 1
 sleep $DELAY
-adb shell input keyevent 4
+adb -e emu finger touch 1
 sleep $DELAY
-adb shell input keyevent 4
-sleep $DELAY
-adb shell input keyevent 4
-sleep $DELAY
-adb shell input keyevent 4
-sleep $DELAY
-adb shell input keyevent 4
-sleep $DELAY
-adb shell input keyevent 4
+adb shell input keyevent KEYCODE_HOME
