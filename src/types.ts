24b2cf8298cd7502dd985cf26909fbb779b4b6e9
MazurDorian <mazur.dorian15@gmail.com>
2024-10-20 17:49:37 +0300

    chore: split index.ts to multiple files and improve docs

---
+++ src/types.ts
@@ -0,0 +1,110 @@
+import type {
+  ACCESS_CONTROL,
+  ACCESSIBLE,
+  AUTHENTICATION_TYPE,
+  SECURITY_LEVEL,
+  SECURITY_RULES,
+  STORAGE_TYPE,
+} from './enums';
+
+/**
+ * Options for authentication prompt displayed to the user.
+ */
+export type AuthenticationPrompt = {
+  /** The title for the authentication prompt. */
+  title?: string;
+  /** The subtitle for the authentication prompt.
+   * @platform Android
+   */
+  subtitle?: string;
+  /** The description for the authentication prompt.
+   * @platform Android
+   */
+  description?: string;
+  /** The cancel button text for the authentication prompt.
+   * @platform Android
+   */
+  cancel?: string;
+};
+
+/** Base options for keychain functions. */
+export type Options = {
+  /** The access control policy to use for the keychain item. */
+  accessControl?: ACCESS_CONTROL;
+  /** The access group to share keychain items between apps.
+   * @platform iOS, visionOS
+   */
+  accessGroup?: string;
+  /** Specifies when a keychain item is accessible.
+   * @platform iOS, visionOS
+   * @default ACCESSIBLE.AFTER_FIRST_UNLOCK
+   */
+  accessible?: ACCESSIBLE;
+  /** Authentication type for retrieving keychain item.
+   * @platform iOS, visionOS
+   * @default AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS
+   */
+  authenticationType?: AUTHENTICATION_TYPE;
+  /** The service name to associate with the keychain item.
+   * @default 'App bundle ID'
+   */
+  service?: string;
+  /** The server name to associate with the keychain item. */
+  server?: string;
+  /** The desired security level of the keychain item. */
+  securityLevel?: SECURITY_LEVEL;
+  /** The storage type.
+   * @platform Android
+   * @default 'Best available storage'
+   */
+  storage?: STORAGE_TYPE;
+  /** The security rules to apply when storing the keychain item.
+   * @platform Android
+   * @default SECURITY_RULES.AUTOMATIC_UPGRADE
+   */
+  rules?: SECURITY_RULES;
+  /** Whether to synchronize the keychain item to iCloud.
+   * @platform iOS
+   */
+  cloudSync?: boolean;
+  /** Authentication prompt details or a title string.
+   * @default
+   * ```json
+   * {
+   *   "title": "Authenticate to retrieve secret",
+   *   "cancel": "Cancel"
+   * }
+   * ```
+   *
+   */
+  authenticationPrompt?: string | AuthenticationPrompt;
+};
+
+/**
+ * Result returned by keychain functions.
+ */
+export type Result = {
+  /** The service name associated with the keychain item. */
+  service: string;
+  /** The storage type used for the keychain item. */
+  storage: STORAGE_TYPE;
+};
+
+/**
+ * User credentials returned by keychain functions.
+ */
+export type UserCredentials = {
+  /** The username associated with the keychain item. */
+  username: string;
+  /** The password associated with the keychain item. */
+  password: string;
+} & Result;
+
+/**
+ * Shared web credentials returned by keychain functions.
+ * @platform iOS
+ */
+export type SharedWebCredentials = {
+  /** The server associated with the keychain item. */
+  server: string;
+} & UserCredentials;
