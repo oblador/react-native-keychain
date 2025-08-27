e7215c9315983037fec3124058884d4a8a0140cf
MazurDorian <mazur.dorian15@gmail.com>
2025-08-27 19:37:03 +0200

    ci: fix e2e

--- KeychainExample/.detoxrc.js
+++ KeychainExample/.detoxrc.js
@@ -43,7 +43,7 @@ module.exports = {
       type: 'ios.simulator',
       headless: Boolean(process.env.CI),
       device: {
-        type: 'iPhone 15 Pro',
+        type: 'iPhone 16 Pro',
       },
     },
     attached: {
