6025e6a36dc744c0e3e6bbeccbbd84abb2978961
Lewis Barnes <lb.barnes@live.co.uk>
2025-09-06 16:33:10 +0100

    Simplified parsing and added reference to root cause.

--- src/KeychainError.ts
+++ src/KeychainError.ts
@@ -1,41 +1,28 @@
 import { ERROR_CODE } from './enums';
-
-interface NativeErrorObject {
-  code: ERROR_CODE;
-  message: string;
-}
-
-const isNativeErrorObject = (err: unknown): err is NativeErrorObject => {
-  if (!err || typeof err !== "object") {
-    return false;
-  }
-
-  return "code" in err && "message" in err;
-};
-
 /**
  * Custom error class for encapsulating the native error objects.
  */
-class KeychainError extends Error {
+export class KeychainError extends Error {
   readonly code: ERROR_CODE;
+  readonly cause: Error | null;
 
-  constructor(message: string, code: ERROR_CODE) {
+  constructor(message: string, code: ERROR_CODE, cause?: Error) {
     super(message);
 
     this.name = 'KeychainError';
     this.code = code;
+    this.cause = cause ?? null;
   }
 
   static parse(err: unknown) {
     if (err instanceof Error) {
-      return err;
+      const code =
+        'code' in err ? (err.code as ERROR_CODE) : ERROR_CODE.INTERNAL_ERROR;
+
+      return new KeychainError(err.message, code, err);
     }
 
-    if (isNativeErrorObject(err)) {
-      return new KeychainError(err.message, err.code);
-    }
-
-    return new KeychainError('An unknown error occurred', ERROR_CODE.INTERNAL_ERROR);
+    return err;
   }
 }
 
